9bba2ea7edb3876753bb1784ce163edc84be8846
Tim Rice <timrice@gmail.com>
2014-06-11 18:19:23 -0400

    client side todo create

--- public/js/controllers/todo.controller.js
+++ public/js/controllers/todo.controller.js
@@ -3,5 +3,19 @@ angular.module('controllers.TodoCtrl', ['services.TodoService'])
     '$scope', 'Todos',
     function ($scope, Todos) {
       $scope.todos = Todos.query();
+
+      $scope.add = function (text) {
+        var todo = new Todos({
+          text: text,
+          completed: false
+        });
+
+        todo.$save(function (success) {
+          $scope.todos.push(todo);
+        }, function (error) {
+          console.log(error);
+        });
+      };
+
     }
   ]);
